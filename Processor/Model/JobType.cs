5e5da1571fcb07c31aeab191bfcae78f34ecf391
Andrii Polubinskyi <110838885+AndriiStarRez@users.noreply.github.com>
2023-04-03 15:19:34 +0300

    removed all

--- Processor/Model/JobType.cs
+++
@@ -1,9 +0,0 @@
-namespace CloudFabric.JobHandler.Processor.Model;
-
-public class JobType
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? AssemblyName { get; set; }
-}
-
